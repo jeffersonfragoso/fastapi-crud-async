89ea4e284c1a2f1d7676a4bd91c51960d4e20486
C1310845 Jefferson Fragoso De Souza <c1310845@interno.bb.com.br>
2020-07-22 17:24:59 -0300

    Add teste para busca de id inexistente

--- src/tests/test_notes.py
+++ src/tests/test_notes.py
@@ -44,6 +44,17 @@ def test_read_note(test_app, monkeypatch):
     assert response.json() == test_data
 
 
+def test_read_note_incorrect_id(test_app, monkeypatch):
+    async def mock_get(id):
+        return None
+
+    monkeypatch.setattr(crud, "get", mock_get)
+
+    response = test_app.get("/notes/999")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Note not found"
+
+
 def test_read_all_notes(test_app, monkeypatch):
     test_data = [
         {"title": "something", "description": "something else", "id": 1},
